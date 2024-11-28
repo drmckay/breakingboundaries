60d13caa9754504e247070b7a7286ea70dd8216a
Peter Veres <pveres@dmk.sh>
2024-11-28 14:02:02 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,17 +13,17 @@ export default defineConfig({
 				github: 'https://github.com/drmckay',
 			},
 			sidebar: [
-				{
+				/*{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						//{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
-				},
+				},*/
 			],
 		}),
 	],
